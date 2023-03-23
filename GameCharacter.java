9bf5ad4d4048b119de306ef000c449fee7d48ba0
master <pnayimbila@st.ug.edu.gh>
2023-03-23 14:22:54 +0000

    Added signature method for the class

--- GameCharacter.java
+++ GameCharacter.java
@@ -3,4 +3,9 @@ abstract public class GameCharacter {
     //Creates the attributes of the GameCharacter class
     private String name;
     private int hitPoints, damagePoints;
+
+    //Creates a signature method for the class
+    public GameCharacter(){
+
+    }
 }

cfc6ba6b28a5a66c8b75d468a121938c0f47a668
master <pnayimbila@st.ug.edu.gh>
2023-03-27 18:25:47 +0000

    Added a getter and setter for the type attribute

--- Enemy.java
+++ Enemy.java
@@ -1,2 +1,14 @@
-public class Enemy {
+public class Enemy extends GameCharacter{
+
+    //Declares attributes of the class
+    private String type;
+
+    //Creates a getter and setter for the type attribute
+    public String getType(){
+        return this.type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
 }
